2b2a6f0b2141ca09198fafb333178f6e2ff96d27
Chris Lee <minjaelee0727@gmail.com>
2021-11-20 13:14:58 -0500

    init

---
+++ opiCall/Shared/opiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  opiCallApp.swift
+//  Shared
+//
+//  Created by Min Jae Lee on 2021-11-20.
+//
+
+import SwiftUI
+
+@main
+struct opiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
